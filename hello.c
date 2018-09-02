3813f2136fa266e2046f96e3e43113eff8ef5278
lonelyjesse <771048506@qq.com>
2018-09-02 10:52:19 +0800

    hello.c:while(i > 0)

--- hello.c
+++ hello.c
@@ -5,6 +5,12 @@ void main(void)
     int i;
     
     i = 5;
+    while (i > 0)
+    {
+        printf("i = %d\n", i);
+        i--;
+
+    }
     printf("i = %d", i);
     printf("hello world!");
 }
